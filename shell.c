13bae319efb23f1d05056b9902d69e89171f8194
Kohin <kohin1999@hotmail.com>
2021-02-06 16:57:39 -0500

    small mistake

--- shell.c
+++ shell.c
@@ -14,7 +14,7 @@ tokenlist *get_tokens(char *input);
 tokenlist *new_tokenlist(void);
 void add_token(tokenlist *tokens, char *item);
 void free_tokens(tokenlist *tokens);
-char *expand_dollar_sign(char *token);
+char *expand_dollar_sign(char *var);
 
 int main()
 {
